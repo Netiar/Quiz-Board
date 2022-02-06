469ef93a0880c68974f3e8dfeb44b63b924253bb
netiar <collins.odinga@student.moringaschool.com>
2022-02-07 01:08:10 +0300

    feature:-added second javascript function

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ function check(){
     if (q1=="Coby") (a++)
     document.write(a);
 }
-    
+function check(){
+    var c=0;
+    var q2=document.quiz.question2.value;
+    if (q2=="foosha village") (c++)
+    document.write(c);
+}   
