ecd548fca98b772eb2bd28f16232e707f8f2bac4
netiar <collins.odinga@student.moringaschool.com>
2022-02-07 08:31:04 +0300

    feature:feature-added readme

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ function check(){
     var q3=document.quiz.question3.value;
     var q4=document.quiz.question4.value;
     var q5=document.quiz.question5.value;
-    var result= document.getElementById("result")
-    var quiz=document.getElementById("quiz")
+    var result= document.getElementById("result");
+    var quiz=document.getElementById("quiz");
     if (q1=="Coby") {a++}
     if (q2=="foosha village") {a++}
     if (q3=="alvinda") {a++}
     if (q4=="Zoro") {a++}    
     if (q5=="pirate hunter") {a++}
 
-    quiz.style.display="none"
-    result.textContent="$(c)";
+    quiz.style.display="none";
+    
+    /*if(c<=3) {
+     result.textContent='your result is $(c).watch one piece.'
+    } else{
+     result.textContent='your result is $(c).An emperor of the sea.'
+    }*/
 }    
 
 
